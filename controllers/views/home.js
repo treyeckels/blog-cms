ef40e4405d339e9ad4efb76194df0557e3940af0
Trey Eckels <trey.eckels@gmail.com>
2021-09-25 23:07:22 -0400

    Homepage and blog card partial.

--- controllers/views/home.js
+++ controllers/views/home.js
@@ -1,7 +1,29 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.render("home");
+  const blogs = [
+    {
+      title: "First Blog",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacus diam, vehicula id elementum sit amet, accumsan at tellus. Donec sed lobortis lectus. Quisque dapibus lacinia orci, iaculis sagittis nisl pharetra eu. Cras sollicitudin lacinia lacus nec semper. Duis nec ligula eros. Phasellus bibendum mauris vel laoreet condimentum.",
+      user: {
+        userName: "teckels",
+        name: "Trey Eckels",
+      },
+      createdAt: "YYYY-MM-DD HH:MM:SS",
+    },
+    {
+      title: "Second Blog",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacus diam, vehicula id elementum sit amet, accumsan at tellus. Donec sed lobortis lectus. Quisque dapibus lacinia orci, iaculis sagittis nisl pharetra eu. Cras sollicitudin lacinia lacus nec semper. Duis nec ligula eros. Phasellus bibendum mauris vel laoreet condimentum.",
+      user: {
+        userName: "teckels",
+        name: "Trey Eckels",
+      },
+      createdAt: "YYYY-MM-DD HH:MM:SS",
+    },
+  ];
+  res.render("home", { blogs });
 });
 
 module.exports = router;
